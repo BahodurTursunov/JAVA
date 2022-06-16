9b8dea781c82951adbecdf06e1fc5038d00aed98
bakhodur <bahodur0101@icloud.com>
2022-06-16 22:52:57 +0300

    hello world and human class

---
+++ lesson4/src/helloworld.java
@@ -0,0 +1,15 @@
+package src;
+
+class Human {
+    public static void main(String[] args) {
+        Human human = new Human();
+        human.buyBred();
+        human.buyMilk();
+    }
+    void buyBred(){
+        System.out.println("kupil xleb");
+    }
+    void buyMilk(){
+        System.out.println("kupil moloko");
+    }
+}
